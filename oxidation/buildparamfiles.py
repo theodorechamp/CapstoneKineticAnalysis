13beb415c8f3d33cc62b1303b6a47688f2ba1001
Jason DesVeaux <jsd2498@gmail.com>
2020-02-23 11:24:28 -0700

    New json capabilities

--- oxidation/buildparamfiles.py
+++ oxidation/buildparamfiles.py
@@ -1,14 +1,31 @@
-import json
+import json, codecs
 import math as m
 import os
+from numpy.random import randint
 
-def jsonoutput(params, fn):
+def jsonoutput(params, testCaseID, fn):
+    #If file doesn't exist, initialize with [] for json --> add later
     savelocation = os.getcwd() + "/paramfiles/" + fn
     with open(savelocation,"w") as f_obj:
-        json.dump(params, f_obj)
+        # if not (f_obj[testCaseID]):
+        #     f_obj[testCaseID] = []
+        # f_obj[testCaseID].append(params)
+        data = {}
+        data[testCaseID] = []
+        data[testCaseID].append(params)
+        print(data)
+        json.dump(data, f_obj)
 
+def randparams(params):
+    testCaseID = "test"
+    for key in params:
+        test = 0
+        params[key] = randint(20)
+        print(params)
+    return ([params, testCaseID])
 
 def main():
+    testCaseID = "YA"
     params = {
         'k1':10**-1.55,
         'gamma1':2.5,
@@ -20,7 +37,9 @@ def main():
         'conversion':.5,
         'tspan':10,
         'tstep':.1 }
-    jsonoutput(params,"testparams.json")
+
+    [params, testCaseID] = randparams(params)
+    jsonoutput(params,testCaseID,"testparams.json")
 
 
 
