84b660833089e79dc43adc505e4ed1441ae66ec8
desveauxj <jsd2498@gmail.com>
2020-02-26 17:16:30 -0700

    trying to merge in a way

--- oxidation/buildparamfiles.py
+++ oxidation/buildparamfiles.py
@@ -1,16 +1,48 @@
-import json
+import json, codecs
 import math as m
 import os
+from numpy.random import randint
+class jsonComm(object):
+    """docstring for jsonComm."""
+    def __init__(self):
+        super(jsonComm, self).__init__()
+        if(open("params.json","x"))
+            self.f_obj = open("params.json","+")
+            data = {"lastUID":0, "iterations":[]}
+            json.dump(data, f_obj)
+        else
+            self.f_obj = open("params.json", "+")
 
-def jsonoutput(params, fn):
-    savelocation = os.getcwd() + "/paramfiles/" + fn
-    with open(savelocation,"w") as f_obj:
-        json.dump(params, f_obj)
 
+    def createfile():
+        test = 1
+
+    def jsonoutput(params, testCaseID, fn):
+        #If file doesn't exist, initialize with [] for json --> add later
+        # if not (f_obj[testCaseID]):
+        #     f_obj[testCaseID] = []
+        # f_obj[testCaseID].append(params)
+        data = {}
+        data[testCaseID] = []
+        data[testCaseID].append(params)
+        print(data)
+        json.dump(data, f_obj)
+
+    def getUID():
+        f = json.load(f_obj)
+        UID = f['lastUID']+1
+        return(UID)
+
+def randparams(params):
+    for key in params:
+        test = 0
+        params[key] = randint(20)
+        print(params)
+    return ([params, testCaseID])
 
 def main():
     params = {
-        'k1':10**-1.55,
+        'k1':10**-1.55,1,10],
         'gamma1':2.5,
         'n1':2.0,
         'k3':10**-1,
@@ -19,11 +51,18 @@ def main():
         'Ftot':5,
         'conversion':.5,
         'tspan':10,
-        'tstep':.1 }
-    jsonoutput(params,"testparams.json")
+        'tstep':.1,
+        'mH': 70,
+        'MWH':120,
+        'alpha0':5,
+        'Vr': 10,
+        'tau':1,
+        'n':1,
+        'x':1 }
+    j = jsonComm
 
-
-
+    params = randparams(params)
+    jsonoutput(params,testCaseID,"testparams.json")
 
 if __name__ == "__main__":
     main()
