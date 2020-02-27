ab1385ed95757b91475f3485750b373ec1c3471f
theodorechamp <theodore.champ@colorado.edu>
2020-02-27 11:33:12 -0700

    Cleaning up code

--- oxidation/fun_conversionoxidation.py
+++ oxidation/fun_conversionoxidation.py
@@ -4,7 +4,7 @@ def conversion(t, y0, p):
     numvariables = 4
     y = {'conv':0, 'nvac':0, 'nh2':0, 'nco': 0}
     dadt = p.conversion / p.tspan * p.tstep #maybe change later
-    Yco = (p.n * p.tau / p.Vr * dadt
+    Yco = (p.n * p.tau / p.Vr * dadt) #change to reactor
     y['conv'] = p.k1 * Yco**p.gamma1 * (1-y0['conv'])**p.n1 +\
     p.k3 * Yco**p.gamma3 * (1-y0['conv'])**p.n1
     y['nvac'] = - y['conv'] * (4 * p.mH/p.mw - p.delta*p.m/p.mw)
