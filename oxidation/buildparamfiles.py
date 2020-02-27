38c767752989be2d98f5d6c793a70bed63a62363
desveauxj <jsd2498@gmail.com>
2020-02-27 00:08:29 -0700

    Update buildparamfiles.py

--- oxidation/buildparamfiles.py
+++ oxidation/buildparamfiles.py
@@ -6,39 +6,27 @@ class jsonComm(object):
     """docstring for jsonComm."""
     def __init__(self):
         super(jsonComm, self).__init__()
-        if(open("params.json","x"))
-            self.f_obj = open("params.json","+")
+        try:
+            open("params.txt",'x')
+            self.f = open("params.txt", 'r+')
             data = {"lastUID":0, "iterations":[]}
-            json.dump(data, f_obj)
-        else
-            self.f_obj = open("params.json", "+")
-
-<<<<<<< HEAD
-def jsonoutput(params, fn):
-    savelocation = os.getcwd() + "/paramfiles/" + fn
-    with open(savelocation,"a") as f_obj:
-        json.dump(params, f_obj)
-=======
->>>>>>> b808caf61111adde11e3eede02cfa5e5a072d535
-
-    def createfile():
-        test = 1
-
-    def jsonoutput(params, testCaseID, fn):
-        #If file doesn't exist, initialize with [] for json --> add later
-        # if not (f_obj[testCaseID]):
-        #     f_obj[testCaseID] = []
-        # f_obj[testCaseID].append(params)
-        data = {}
-        data[testCaseID] = []
-        data[testCaseID].append(params)
+            json.dump(data, self.f, indent=2)
+        except FileExistsError:
+            self.f = open("params.txt", 'r')
+            data = json.load(self.f)
+        self.fs = data
         print(data)
-        json.dump(data, f_obj)
+        self.f = open("params.txt", 'w')
 
-    def getUID():
-        f = json.load(f_obj)
-        UID = f['lastUID']+1
-        return(UID)
+
+    def jsonoutput(self,params):
+        print(self.fs)
+        UID = self.fs["lastUID"]+1
+        self.fs["lastUID"] = UID
+        data = {"UID":UID,"params":params}
+        self.fs["iterations"].insert(0,data)
+        print(self.fs)
+        json.dump(self.fs,self.f,indent=2)
 
 def randparams(params):
     for key in params:
@@ -49,32 +37,28 @@ def randparams(params):
 
 def main():
     params = {
-<<<<<<< HEAD
         'k1':10**-1.55,
-        'gamma1':2,
-=======
-        'k1':10**-1.55,1,10],
         'gamma1':2.5,
->>>>>>> b808caf61111adde11e3eede02cfa5e5a072d535
         'n1':2.0,
         'k3':10**-1,
         'n3':1.5,
-        'Vco':10,
+        'Vco':11,
         'Ftot':5,
         'conversion':.5,
         'tspan':10,
         'tstep':.1,
         'mH': 70,
         'MWH':120,
-        'alpha0':5,
+        'delta':5,
         'Vr': 10,
         'tau':1,
         'n':1,
         'x':1 }
-    j = jsonComm
+    j = jsonComm()
+    j.jsonoutput(params)
 
-    params = randparams(params)
-    jsonoutput(params,testCaseID,"testparams.json")
+    # params = randparams(params)
+    # jsonoutput(params,testCaseID,"testparams.json")
 
 if __name__ == "__main__":
     main()
