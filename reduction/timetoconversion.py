f58d2cff3f412aeed244a0fc4b7641ad5077bf33
anch9549 <56856591+anch9549@users.noreply.github.com>
2020-03-01 11:48:45 -0700

    updated file has different name

--- reduction/timetoconversion.py
+++
@@ -1,7 +0,0 @@
-import math as m
-
-def timetoconversion(p):
-    fint = -2/(1-p.conversion)^2 + 2 # the integral of the f(alpha) fxn
-    pdep = 1 # add in after we find the pressure dependence; may need to take integral of dependence
-    rhs = p.A*m.exp(-p.Ea/(p.R*p.T))
-    time = (fint/pdep)/rhs
