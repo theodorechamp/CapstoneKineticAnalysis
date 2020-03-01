788416a3fb511154d449c9cc2023ffed765cd62f
Jason DesVeaux <jsd2498@gmail.com>
2020-02-29 18:10:14 -0700

    Removed test files and print code, added randomization

--- oxidation/buildparamfiles.py
+++ oxidation/buildparamfiles.py
@@ -1,7 +1,7 @@
 import json, codecs
 import math as m
 import os
-from numpy.random import randint
+import random
 class jsonComm(object):
     """docstring for jsonComm."""
     def __init__(self):
@@ -16,49 +16,48 @@ class jsonComm(object):
             self.f = open(self.fn, 'r')
             data = json.load(self.f)
         self.fs = data
-        print(data)
         self.f = open(self.fn, 'w')
 
-
     def jsonoutput(self,params):
-        print(self.fs)
         UID = self.fs["lastUID"]+1
         self.fs["lastUID"] = UID
         data = {"UID":UID,"params":params}
         self.fs["iterations"].insert(0,data)
-        print(self.fs)
         json.dump(self.fs,self.f,indent=2)
 
 def randparams(params):
-    for key in params:
-        test = 0
-        params[key] = randint(20)
-        print(params)
-    return ([params, testCaseID])
+    for item in params:
+        key = params[item]
+        key[0] = random.uniform(key[1],key[2])
+    return (params)
 
 def main():
-    params = {
+    constants = {
         'k1':10**-1.55,
         'gamma1':2.5,
         'n1':2.0,
         'k3':10**-1,
         'n3':1.5,
-        'Vco':11,
-        'Ftot':5,
-        'conversion':.5,
-        'tspan':10,
-        'tstep':.1,
-        'mH': 70,
-        'MWH':120,
-        'delta':5,
-        'Vr': 10,
-        'tau':1,
-        'n':1,
-        'x':1 }
+        'tstep':.1
+    }
+    params = {
+        'Vco':[11,1,10],
+        'Ftot':[5,1,10],
+        'conversion':[.5,0,1],
+        'tspan':[10,1,100],
+        'mH': [70,60,100],
+        'MWH':[120,100,200],
+        'delta':[5,1,10],
+        'Vr': [10,1,100],
+        'tau':[10,1,10],
+        'n':[1,1,10],
+        'x':[1,1,10]
+    }
     j = jsonComm()
+    params = randparams(params)
     j.jsonoutput(params)
 
-    # params = randparams(params)
+    #
     # jsonoutput(params,testCaseID,"testparams.json")
 
 if __name__ == "__main__":
