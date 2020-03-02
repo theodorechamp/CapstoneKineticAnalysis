67f36d394d66e4903cb1392d1572f6437f3996d5
theodorechamp <theodore.champ@colorado.edu>
2020-03-02 11:06:49 -0700

    More progress on oxidation

--- oxidation/execODE.py
+++ oxidation/execODE.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 import json
-
-
+from fun_conversionoxidation import *
 
 def execODE(params):
-    print(params)
+    nvac = params.
+    y0 = {
+        'conv':0,
+        'nvac':.25
+
+    }
+    sol = solve_ivp(conversion, [0, 10],  args = params)
