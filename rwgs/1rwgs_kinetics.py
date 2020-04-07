3255fd471fd2214c5d55601005fae78b75d1ef16
root <root@DESKTOP-33QD3D4.localdomain>
2020-04-07 12:59:03 -0600

    added addnl parameter to the rate expr. working real naice.

--- rwgs/1rwgs_kinetics.py
+++ rwgs/1rwgs_kinetics.py
@@ -9,59 +9,78 @@ def mflow(n, MW):
     for i in range(len(MW)):
         mflowTot = mflowTot + n[i]*MW[i]
     return(mflowTot)
+
 #Ergun equation
 def ergun(p, MW, T, R, mflowTot, D):
     rho = 0 #total density of the gas mixture
     for i in range(len(p)):
-        rho = rho + p[i]*MW[i]/(T*R) #g/L
+        rho = rho + p[i]*MW[i]/(T*R) #g/L = kg/m**3
     phi = 0.3 #void fraction
     Dp = D/8 #[] = m. Particle diameter. Using heuristic Weimer gave in class on 10/10/19 that particle diameter should be 1/8 of tube diameter
     Ac = math.pi*(D**2)/4 #[] = m^2. cross sectional area
-    G0 = mflowTot/Ac #g/hr/m^2. mass flux
-    mu = 3.6*0.01849 #3.6 = unit conversion. Value from HYSYS. [] = kg m**-1 hr**-1
-    #differential pressure change
-    dPdV = -150/Ac*(mu*(1-phi)/(Dp*G0)+(7/4))*((1-phi)/phi**3)*(G0**2/(rho*Dp))
-    #conert units from kg m**-4 hr**-2 to atm L**-1
-    dPdV = dPdV/(1000**3)/(3600**2)/(101325)
+    G0 = mflowTot/Ac/3600 #g/s/m^2. mass flux
+    mu = 3.6*0.01849/3600 #3.6 = unit conversion. Value from HYSYS. [] = kg m**-1 s**-1
+    #differential pressure change. want all units in SI.
+    dPdV = -G0*(1-phi)/rho/Ac/Dp/(phi**3)*(150*mu*(1-phi)/Dp + 7/4*G0)
+    #convert units from kg m**-4 hr**-2 to atm L**-1
+    dPdV = dPdV/(1000**3)/(101325)
     return(dPdV)
 
-# f(t, y, c)
-# y = [nCO2, nH2, nCO, nH2O, P]
+# solve_ivp syntax: f(t, y, c)
+# our y = [nCO2, nH2, nCO, nH2O, P]
 def reaction(V, y, C):
-    # unpacking C = [P0CO2, P0H2, k1L0, K2, K3, V[1], MW, T, R, mflowTot, D]
-    P0CO2, P0H2, k1L0, K2, K3, dV, MW, T, R, mflowTot, D = C
-    #calculate p from y = [nCO2, nH2, nCO, nH2O, Ptot]
+    # unpacking C
+    P0CO2, P0H2, dV, MW, T, R, mflowTot, D, Vtot = C
+    #cap each species at zero
+    for i in range(0,4):
+        if y[i] <= 0:
+            y[i] = 0
+    #calculate p's from y = [nCO2, nH2, nCO, nH2O, Ptot]
     ntot = 0
-    gCat = 3950/2
-    Ptot = y[4]
     for i in range(0,4):
         ntot = ntot + y[i]
     p = [0]*4
     for i in range(0,4):
         p[i] = y[i]/ntot*y[4]
+        if y[i] <= 0:
+            p[i], y[i] = 0, 0
+    #parameters needed to find rate
+    #kinetic parameters. taken from results of Gines et al., 1997
+    #k1L0 = (rate of fwd CO2 dissoc.)*(conc. of empty catalyst sites)
+    k1L0 = 0.1287 #units = mol/h/gcat/atm**2
+    K2 = 0.039 #equilibrium constant for H2 adsoprtion
+    K3 = 0.00000101 #equilibrium constant for H2O dissociation
     K = p[2]*p[3]/p[0]/p[1]
-    print('K: ' + str(K))
     X = (P0CO2 - p[0])/P0CO2
-    #rate units = mol/gcat/hr
-    r = k1L0*P0CO2*(P0H2*((1-X)**2) - P0CO2*(X**2)/K)
+    if X >= 1:
+        X = 1
+    #rate [=] mol gCat**-1  hr**-1
+    print('V: ' + str(V))
+    print('K: ' + str(K))
+    print('X: ' + str(X))
+    print('Term1: ' + str(P0H2*((1-X)**2)))
+    print('Term2: ' + str(P0CO2*(X**2)/K))
+    # print('Term3: ' + str((P0H2*(1-X))))
+    # print('Term4: ' + str((K2**0.5)*P0H2*1.5*(((1-X)**1.5))))
+    # print('Term5: ' + str(P0CO2*X/K2/K3))
+    F = 0.00001 #parameter to fix erroneous rate
+    r = k1L0*P0CO2*(P0H2*((1-X)**2)/F - F*P0CO2*(X**2)/K)
     r = r/(P0H2*(1-X) + (K2**0.5)*P0H2*1.5*(((1-X)**1.5)) + P0CO2*X/K2/K3)
+    # r = 10
     print('rate: ' + str(r))
-    #first four entries are dn/dV, 5th is dPtot/dV
-    dydV = [-1*r*gCat*Ptot/(ntot*R*T), -1*r*gCat*Ptot/(ntot*R*T),
-            1*r*gCat*Ptot/(ntot*R*T), 1*r*gCat*Ptot/(ntot*R*T),
-            dV*ergun(p, MW, T, R, mflowTot, D)]
+    #parameters for calculating dydV
+    mCat = 3950*Vtot #cat. loading = Al2O3 density multiplied by vol of 1 tube
+    Ptot = y[4]
+    # dn/dV = dn/dt/Vdot = dn/dt/ndot/R/T*Ptot = nu*r*gCat*Ptot/(ndot*R*T)
+    # first four entries are dn/dV, 5th is dPtot/dV
+    # dydV = [-1*r*mCat*dV*Ptot/(ntot*R*T), -1*r*mCat*dV*Ptot/(ntot*R*T),
+    #         1*r*mCat*dV*Ptot/(ntot*R*T), 1*r*mCat*dV*Ptot/(ntot*R*T),
+    #         ergun(p, MW, T, R, mflowTot, D)]
+    dydV = [-1*r*mCat*Ptot/(ntot*R*T), -1*r*mCat*Ptot/(ntot*R*T),
+            1*r*mCat*Ptot/(ntot*R*T), 1*r*mCat*Ptot/(ntot*R*T),
+            ergun(p, MW, T, R, mflowTot, D)]
+    print('dydV: ' + str(dydV))
     return(dydV)
-
-# OVERALL REACTION: CO2 + H2 -> CO + H2O
-# We have rate r, [r]=mol/hr/g catalyst
-# dnCO2/dt = -r*gCat
-# dnH2/dt = -r*gCat
-# dnCO/dt = r*gCat
-# dnH2O/dt = r*gCat
-#
-# dn/dV = dn/dt/Vdot = dn/dt/ndot/R/T*Ptot = nu*r*gCat*Ptot/(ndot*R*T)
-#
-# y(t) = [nCO2[x1,x2,x3], nH2, nCO, nH2O]
 
 def plotdata(V, sol):
     y = sol.y
@@ -72,8 +91,8 @@ def plotdata(V, sol):
     plt.figure(1)
     plt.plot(V, CO2, 'r', label = 'CO2')
     plt.plot( V, H2, 'g', label = 'H2')
-    plt.plot( V, CO, 'b', label = 'CO')
-    plt.plot( V, H2O, 'm', label = 'H2O')
+    plt.plot( V, CO, 'm', label = 'CO')
+    plt.plot( V, H2O, 'b', label = 'H2O')
     plt.xlabel('Volume (L)')
     plt.ylabel('Flowrate (mol/hr)')
     plt.legend()
@@ -82,22 +101,29 @@ def plotdata(V, sol):
 
 def main():
     #constant parameters
-    R = 0.08205 #gas constant. units = L atm K^-1 mol^-1
-    T = 523 #temperature. units = K
+    R = 0.08205 #gas constant. [=] L atm K**-1 mol**-1
+    T = 523 #isothermal temperature. [=] K
+    Ptot = 44*1.01325 # feed pressure. units converted from bar to atm
+    #reactor dimensions
+    D = 0.0254 #tube diameter. determined from Aboudheir et al. [=] m
+    L = 1 #tube length. [=] m
+    Vtot = 1000*L*math.pi*(D**2)/4 #total volume of 1 tube. units converted to L
+
     #molecular weight dictionary for all species
     MWDict = {'CO2': 44.008, 'H2': 1.008, 'CO': 28.008, 'H2O': 18.016}
     MW = list(MWDict.values()) #create list instead of dictionary for calculations
 
-    #dictionary for feed molar flowrates of each species. units = mol/hr
-    nDict = {'CO2':250, 'H2': 749.98, 'CO': 0.01, 'H2O': 0.01}
+    #feed molar flowrates of each species. [=] mol/hr
+    ratio = 3
+    nDict = {'CO2': 25, 'H2': 1, 'CO': 0.001, 'H2O': 0.001}
+    nDict['H2'] = ratio*nDict['CO2'] - nDict['CO'] - nDict['H2O']
     n = list(nDict.values())
     ntot = 0 #total molar flowrate
     for i in range(len(n)):
         ntot = ntot + n[i]
-    mflowTot = mflow(n, MW)
-    #feed mole fraction dictionary.
+    mflowTot = mflow(n, MW) #total mass flow
+    #feed mole fraction dictionary
     yDict = {'CO2': nDict['CO2']/ntot, 'H2': nDict['H2']/ntot, 'CO': nDict['CO']/ntot, 'H2O': nDict['CO2']/ntot}
-    Ptot = 33*1.01325 #feed pressure. units converted from bar to atm
     #initial partial pressures. units = atm
     pDict = {'CO2': Ptot*yDict['CO2'],
             'H2': Ptot*yDict['H2'],
@@ -109,27 +135,29 @@ def main():
     P0H2 = pDict['H2']
     p = list(pDict.values()) #create list instead of dictionary for calculations
 
-    #kinetic parameters. taken from results of Gines et al., 1997
-    #k1L0 = (rate of fwd CO2 dissoc.)*(conc. of empty catalyst sites)
-    k1L0 = 0.1287 #units = mol/h/gcat/atm**2
-    K2 = 0.039 #equilibrium constant for H2 adsoprtion
-    K3 = 0.125
-    #K3 = 0.00000125 #equilibrium constant for H2O dissociation
-    X = 0.0000001 #conversion (initial)
-    K = pDict['CO']*pDict['H2O']/pDict['CO2']/pDict['H2'] #overall equilibrium constant
-    rate = k1L0*pDict['CO2']/(1 + (K2*pDict['H2'])**0.5) #RWGS rxn rate
-
-    #reactor dimensions.
-    D = 0.0254 #tube diameter. units = m
-    L = 1 #tube length. units = m
-    Vtot = 1000*L*math.pi*(D**2)/4 #total volume of 1 tube. units converted to L
-    V = numpy.linspace(0,Vtot,101)
-
-    # y0 = [nCO20, nH20, nCO0, nH2O0, P0]
-    C = [P0CO2, P0H2, k1L0, K2, K3, V[1], MW, T, R, mflowTot, D]
+    #list of constant args needed for f(t, y, C)
+    V = numpy.linspace(0, Vtot, 101)
+    #dV = V[1]
+    C = [P0CO2, P0H2, V[1], MW, T, R, mflowTot, D, Vtot]
+    # y0 = [nCO20, nH20, nCO0, nH2O0, P0] = molar flowrates, and total pressure.
     y0 = [ nDict['CO2'], nDict['H2'], nDict['CO'], nDict['H2O'], Ptot]
-    #(V, y, P0CO2, P0H2, k1L0, K2, K3, dV, MW, T, R, mflowTot, D)
+    #(V, y, P0CO2, P0H2, k1L0, K2, K3, dV, MW, T, R, mflowTot, D, Vtot)
     sol = solve_ivp(lambda t, y: reaction(t,y,C), [V[0], V[-1]], y0, t_eval = V)
+    # print('sol: ' + str(sol))
+    print('sol.y[0]: ' + str(sol.y[0]))
+
+    #finding total pressure at reactor exit
+    dPdVtot = 0
+    for i in range(len(V)):
+        dPdVtot = dPdVtot + V[1]*sol.y[4][i]
+    print('dPdVtot: ' + str(dPdVtot))
+    Pfinal = 44*1.01325 - dPdVtot*Vtot
+    print('Pfinal: ' + str(Pfinal))
+
+    #final conversion
+    XCO2 = (nDict['CO2'] - sol.y[0][-1])/nDict['CO2']
+    print('XCO2: ' + str(XCO2))
+
     plotdata(V, sol)
 
 if __name__ == '__main__':
